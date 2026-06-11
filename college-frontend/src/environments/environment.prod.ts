1fc803de6f62ca40aeb9b410830fcf042be7b8c4
feminrajput <feminrajput@gmail.com>
2026-06-11 19:03:07 +0530

    config: update production backend API URL

---
+++ college-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://college-admission-management.onrender.com/api',
+    staticUrl: 'https://college-admission-management.onrender.com',
+    firebase: {
+        apiKey: "AIzaSyAk31PdKkWzPxBamebIdgaxjq6Up7o7mJs",
+        authDomain: "college-portal-f1ec8.firebaseapp.com",
+        projectId: "college-portal-f1ec8",
+        storageBucket: "college-portal-f1ec8.firebasestorage.app",
+        messagingSenderId: "429845659816",
+        appId: "1:429845659816:web:99d3a74c63a63a813bc29d",
+        measurementId: "G-PT59P9RE7C"
+    }
+};
